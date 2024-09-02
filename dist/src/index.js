aa3c05ae91bdbebf1e29e0ac4e0deece919f323e
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-02 20:57:09 +0100

    add objects and enums

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,6 +1,31 @@
 "use strict";
-let age = 33;
-if (age < 50) {
-    age += 10;
+let sale = 123456789;
+let course = "TypeScript";
+let isPublished = true;
+let thisNumber = 123432;
+let type;
+function render(document) {
+    console.log(document);
 }
+let numbers = [];
+let user = [1, "Luke"];
+user.push(3);
+let mySize = 3;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    else {
+        return income * 1.3;
+    }
+}
+calculateTax(10000, 2023);
+let employee = { id: 1,
+    name: "Luke",
+    askForRaise: (str) => {
+        console.log(str);
+    }
+};
+employee.askForRaise("Please can I have a raise?");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,32 @@ let numbers: number[] = []
 
 //Tuples
 let user: [number, string] = [1, "Luke"]
+user.push(3)
+// console.log(user)
+
+// enums
+// const small = 1
+// const medium = 2
+// const large = 3
+
+const enum Size { Small = 1, Medium, Large } 
+let mySize: Size = Size.Large
+console.log(mySize)
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022) {
+        return income * 1.2
+    } else {
+        return income * 1.3
+    }
+}
+
+calculateTax(10_000, 2023)
+
+// Objects Long way
+let employee: {readonly id: number, name: string, askForRaise: (str: string) => void}
+=
+{ id: 1, name: "Luke", askForRaise: (str: string) => {console.log(str)}}
+
+
+employee.askForRaise("Please can I have a raise?")
