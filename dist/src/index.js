0896699d267b5769aa09e5b641a6c3a95f00e4d4
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-02 16:17:43 +0100

    add new data type 'any'

---
+++ dist/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 33;
+if (age < 50) {
+    age += 10;
+}
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-let age: number = 33
+let sale: number = 123_456_789
+let course: string = "TypeScript"
+let isPublished: boolean = true
+
+let thisNumber = 123432 //still a number
+let type // this is type "any"
