08cfec0608e00d4cda794901283f7fdb9d828b51
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-02 16:24:58 +0100

    add array types

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ let course: string = "TypeScript"
 let isPublished: boolean = true
 
 let thisNumber = 123432 //still a number
-let type // this is type "any"
+let type // this is type "any" --- Try avoiding this
+
+function render(document: any) {
+    console.log(document)
+}
+
+let numbers: number[] = []
